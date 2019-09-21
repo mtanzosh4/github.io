618bb6e4a4e5280297101e849cbe3e5c2028eb7b
mtanzosh4 <mtanzosh4@users.noreply.github.com>
2019-09-21 16:54:12 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -1,121 +1,52 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include <ctype.h>
-
-
-int count_letters(string text);
-int count_words(string text);
-int count_sentences(string text);
-
-int Coleman_Liau_index(float letters, float words, float sentences, string text);
+#include <math.h>
 
 int main(void)
 {
-    string text = get_string("Text: ");
+    string s = get_string("Text: ");
 
-    int letters = count_letters(text);
+    float letters = 0;
+    float words = 1;
+    float sentences = 0;
 
-    int words = count_words(text);
-
-    int sentences = count_sentences(text);
-
-    int index = Coleman_Liau_index(letters, words, sentences, text);
-
-    if (index >= 16)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
-        printf("Grade 16+\n");
+        if(islower(s[i]) || isupper(s[i]))
+        {
+            letters++;
+        }
+        if (s[i] == ' ' && s[i+1] != ' ')
+        {
+            words++;
+        }
+        if (s[i] == '.' || s[i] == '!' || s[i] == '?')
+        {
+            sentences++;
+        }
     }
-    else if (index < 1)
+
+    printf("%f letter(s)\n", letters);
+    printf("%f word(s)\n", words);
+    printf("%f sentence(s)\n", sentences);
+
+    float L = 100 * letters / words;
+    float S = 100 * sentences / words;
+    int X = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (X > 16)
     {
-        printf("Before Grade 1\n");
+        printf("Grade 16+");
+    }
+    else if (X < 1)
+    {
+        printf("Before Grade 1");
     }
     else
     {
-        printf("Grade %i\n", index);
+        printf("Grade %i", X);
     }
-
-
+    printf("\n");
 }
-
-// Function to count total amount of letters in the text
-// int letters = count_letters(text);
-// int words = count_words(text);
-// int sentences = count_sentences(text);
-
-int Coleman_Liau_index(float letters, float words, float sentences, string text)
-{
-    // printf("letters: %f\n words: %f\n sentences: %f\n text: %s\n", letters, words, sentences, text);
-    float L = 100.00 / (float) words * (float)letters;
-    float S = 100.00 / (float) words * (float) sentences;
-
-    int index =round(0.0588 * L - 0.296 * S - 15.8);
-    // // printf("index: %f\n", index);
-    // index = round(index);
-    // printf("index: %f\n, L: %f\n, S: %f\n", index, L, S);
-    return index;
-}
-
-// Function to count total amount of letters in the text
-int count_letters(string text)
-{
-    int text_len = strlen(text);
-    int letters_counter = 0;
-
-    for (int i = 0; i < text_len; i ++)
-    {
-        if (isalpha(text[i]))
-        {
-            letters_counter++;
-        }
-    }
-    return letters_counter;
-}
-
-
-// Function to count amount of words in the text
-int count_words(string text)
-{
-    printf("%lu\n", strlen(text));
-    char copy_text[2000];
-    strcpy(copy_text, text);
-
-
-    char *new_array = strtok(copy_text, " ");
-
-    int counter = 0;
-
-    while (new_array != NULL)
-    {
-        new_array = strtok(NULL, " ");
-        counter++;
-    }
-    printf("%i\n", counter);
-    return counter;
-
-}
-
-// Function to count amount of sentences in the text
-int count_sentences(string text)
-{
-
-    int counter = 0;
-    char copy_text2[2000];
-
-    strcpy(copy_text2, text);
-    char *str_array = strtok(copy_text2, ".!?");
-
-    while (str_array != NULL)
-    {
-        str_array = strtok(NULL, ".!?");
-        counter++;
-        // printf("s%i: %s\n addrs: %p\n", counter, str_array, *(&str_array + 1));
-    }
-    printf("count_sentences: %i\n", counter);
-    return counter;
-
-}
-
-
-
