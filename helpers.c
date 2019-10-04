777cf95b8be9b8f3e222ec8aa2d1964e2ece013a
mtanzosh4 <mtanzosh4@users.noreply.github.com>
2019-10-04 16:38:47 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -9,7 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int gray = round((float)image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen) / 3;
+            int gray = round((image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen) / 3.0);
             image[i][j].rgbtBlue = gray;
             image[i][j].rgbtRed = gray;
             image[i][j].rgbtGreen = gray;
