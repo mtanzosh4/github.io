8b1586ef7aef162dec216af689ca7b767c1de34c
mtanzosh4 <mtanzosh4@users.noreply.github.com>
2019-10-05 13:33:48 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,7 +1,6 @@
 // Detects if a file is a JPEG
 
 #include <stdio.h>
-#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
@@ -18,43 +17,48 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    // Read first three bytes
+    // Read first four bytes
     unsigned char bytes[512];
     int file_number = 0;
     char file_name[8];
     sprintf(file_name, "%03i.jpg", file_number);
+
+    // Declare file pointer
     FILE *recoveredfile = fopen(file_name, "w");
-    bool start = false;
-    int bytes_read;
-    while (true)
+
+    while (fread(bytes, 1, 512, rawfile) == 512)
     {
-        bytes_read = fread(bytes, 1, 512, rawfile);
-        if (bytes_read != 512)
-        {
-            // printf("%i bytes\n", bytes_read);
-            break;
-        }
+
         // Check first three bytes
         if (bytes[0] == 0xff && bytes[1] == 0xd8 && bytes[2] == 0xff && (bytes[3] & 0xf0) == 0xe0)
         {
-            start = true;
+
+            // printf("New jpeg!");
+
             // save previous file and start new file
-            if (file_number != 0)
+            if (file_number)
             {
                 fclose(recoveredfile);
-                file_number++;
                 sprintf(file_name, "%03i.jpg", file_number);
                 recoveredfile = fopen(file_name, "w");
             }
+
+            file_number++;
         }
-        if (start)
+
+
+        if (file_number)
         {
-            fwrite(bytes, 1, bytes_read, recoveredfile);
+            fwrite(bytes, 1, 512, recoveredfile);
         }
     }
-    fwrite(bytes, 1, bytes_read, recoveredfile);
+
+    fwrite(bytes, 1, 512, recoveredfile);
     fclose(recoveredfile);
 
     // Close file
     fclose(rawfile);
+
+    // end
+    return 0;
 }
