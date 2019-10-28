70dcfb554f946124f76aa555251e8da3beb02c17
mtanzosh4 <mtanzosh4@users.noreply.github.com>
2019-10-28 15:49:37 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main(void)
 {
@@ -11,28 +12,18 @@ int main(void)
     while(n < 0);
 
     int cents = round(n * 100);
-
     int coins = 0;
-    while (cents >= 25)
-    {
-        cents = cents - 25;
-        coins++;
-    }
-    while (cents >= 10)
-    {
-        cents = cents - 10;
-        coins++;
-    }
-    while (cents >= 5)
-    {
-        cents = cents - 5;
-        coins++;
-    }
-    while (cents >= 1)
-    {
-        cents = cents - 1;
-        coins++;
-    }
+
+    coins += cents/25;
+    cents = cents % 25;
+
+    coins += cents/10;
+    cents = cents % 10;
+
+    coins += cents/5;
+    cents = cents % 5;
+
+    coins += cents;
 
     printf("%i\n", coins);
 }
