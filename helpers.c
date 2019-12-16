5cb245e12b7b09fb349fffce4d36f88a95a2ba84
mtanzosh4 <mtanzosh4@users.noreply.github.com>
2019-12-16 23:17:06 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,87 +1,90 @@
- // Helper functions for music
+// Helper functions for music
 
 #include <cs50.h>
+#include <ctype.h>
 #include <math.h>
+#include <string.h>
 
 #include "helpers.h"
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    int x = fraction[0] - '0';
-    int y = fraction[2] - '0';
-
-    return round(8.0/y * x);
+    int numerator = fraction[0] - '0';
+    int denominator = fraction[2] - '0';
+    return 8.0 * (double) numerator / denominator;
 }
 
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    int octave;
-    char accidental = '\0';
-    char letter = note[0];
-    if (note[1] == '#' || note[1] == 'b')
+    char n[3];
+    int octave = 0;
+    if (strlen(note) == 2)
     {
-        accidental = note[1];
-        octave = note[2] - '0';
-
-    }
-    else
-    {
+        n[0] = note[0];
+        n[1] = '\0';
         octave = note[1] - '0';
     }
+    else if (strlen(note) == 3)
+    {
+        n[0] = note[0];
+        n[1] = note[1];
+        n[2] = '\0';
+        octave = note[2] - '0';
+    }
 
-    int interval;
+    // Calculate relative to A440
+    double f = 440;
 
-    switch (letter) {
+    // Determine how many semitones away to move
+    int semitones = 0;
+    switch (toupper((unsigned char) n[0]))
+    {
         case 'A':
-            interval = 0;
+            octave++;
             break;
         case 'B':
-            interval = 2;
+            semitones = 2;
+            octave++;
             break;
         case 'C':
-            interval = -9;
+            semitones = 3;
             break;
         case 'D':
-            interval = -7;
+            semitones = 5;
             break;
         case 'E':
-            interval = -5;
+            semitones = 7;
             break;
         case 'F':
-            interval = -4;
+            semitones = 8;
             break;
         case 'G':
-            interval = -2;
+            semitones = 10;
             break;
         default:
-            interval = 0;
+            f = 0;
     }
 
-    if (accidental == '#')
+    // Adjust for sharps and flats
+    if (n[1] == '#')
     {
-        interval++;
+        semitones++;
     }
-    else if (accidental == 'b')
+    else if (n[1] == 'b')
     {
-        interval--;
+        semitones--;
     }
 
-    // make interval include octave
-    interval = interval + 12 * (octave - 4);
-
-    int frequency = round(pow(2, interval/12.0) * 440);
-
-    return frequency;
-
-
+    // Compute the final frequency
+    return round(f * pow(2.0, semitones / 12.0 + octave - 5.0));
 }
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-    if (s[0] == '\n' || s[0] == '\0')
+    if (strlen(s) == 0)
     {
         return true;
     }
